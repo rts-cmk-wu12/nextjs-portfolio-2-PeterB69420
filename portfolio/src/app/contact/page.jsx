c07b9d4c5b90e5ce7593c5fd77f7a8dd2727b0e2
Johndillerman <U2486892@rts-365.dk>
2025-06-24 10:37:41 +0200

    font family

--- portfolio/src/app/contact/page.jsx
+++ portfolio/src/app/contact/page.jsx
@@ -6,7 +6,7 @@ export default function Contact() {
         <main className="bg-black min-h-screen">
             <Navbar />
             <section className="flex flex-col items-center">
-                <h2 className="text-white text-center m-8 text-3xl font-bold">Get In Touch</h2>
+                <h2 className="text-white text-center m-8 text-3xl font-bold  ">Get In Touch</h2>
                 <p className="text-[#9C9C9C] text-center max-w-2xl mx-auto">
                     Tak fordi du kiggede forbi!
                     Hvis du leder efter en motiveret og detaljeorienteret webudvikler med interesse for front-end og responsivt design, så vil jeg meget gerne høre fra dig.
